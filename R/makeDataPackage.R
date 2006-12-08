7083280262549c2bd96a35d4c12539f463915249
Seth Falcon <sfalcon@fhcrc.org>
2006-12-08 15:28:50 +0000

    Improve makeDataPackage: add unlink arg, create dir if doesn't exist
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@21392 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeDataPackage.R
+++ R/makeDataPackage.R
@@ -1,13 +1,11 @@
-### =========================================================================
+###
 ### The makeDataPackage method for SQLiteAnnDataPkgSeed objects
-### -----------------------------------------------------------
-### Taken and adapted from pdInfoBuilder/R/makeDataPackage-methods.R
-### hpages@fhcrc.org - 2006-12-07
-
+###
 setMethod(Biobase::makeDataPackage,
     signature(object="SQLiteAnnDataPkgSeed"),
     function(object, author, email, packageName, packageVersion,
-             license, biocViews, filePath, sqliteFilePath, quiet=FALSE)
+             license, biocViews, filePath, sqliteFilePath, quiet=FALSE,
+             unlink=FALSE)
     {
         if (missing(email) || !is.character(email)
          || length(email) != 1 || grep("@", email) != 1)
@@ -31,15 +29,19 @@ setMethod(Biobase::makeDataPackage,
                                    package="AnnotationDbi")
         createPackage(pkgname=packageName, destinationDir=filePath,
                       originDir=templateDir,
-                      symbolValues=syms, quiet=quiet)
+                      symbolValues=syms, quiet=quiet, unlink=unlink)
         dbFilePath <- file.path(extdataDir, dbFileName)
-        file.copy(sqliteFilePath, dbFilePath)
+        if (!dir.create(dbFilePath, showWarnings=FALSE, recursive=TRUE))
+          stop("unable to create directory: ", dbFilePath)
+        if (!file.copy(sqliteFilePath, dbFilePath))
+          stop("unable to copy SQLite DB to destination")
+        return(invisible(TRUE))
     }
 )
 
 ### Just for testing (not exported). Call with
 ###   AnnotationDbi:::make_hgu95av2db("/tmp", "path/to/hgu95av2.sqlite")
-make_hgu95av2db <- function(filePath, sqliteFilePath)
+make_hgu95av2db <- function(filePath, sqliteFilePath, ...)
 {
     pkgseed <- new("SQLiteAnnDataPkgSeed",
                    chipShortName="hgu95av2",
@@ -51,6 +53,6 @@ make_hgu95av2db <- function(filePath, sqliteFilePath)
     makeDataPackage(pkgseed, "Nianhua Li, Seth Falcon", "biocannotation@lists.fhcrc.org",
                     "hgu95av2db", "1.13.999", "LGPL",
                     "AnnotationData, hgu95av2, AffymetrixChip, Homo_sapiens",
-                    filePath, sqliteFilePath)
+                    filePath, sqliteFilePath, ...)
 }
 
