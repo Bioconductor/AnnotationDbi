6911df25464dfc94511e52f79a4a48ccf7e8991a
Seth Falcon <sfalcon@fhcrc.org>
2007-02-05 02:22:03 +0000

    Move all setClass calls to AllClasses.R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@22414 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,3 +1,74 @@
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Classes representing SQLite-based annotation maps.
+###
+
+### An "AnnMap" object is a mapping between left values and right values.
+### The left values are the strings stored in the SQL col 'leftCol' of
+### table 'leftTable'.
+### For direct "AnnMap" objects, the mapping is "left-to-right". The left
+### values are the names (or symbols, or keys) of the map and are retrieved
+### with the "names" or "ls" methods. The type, format and location in the
+### DB of the right values depends on the particular subclass of the "AnnMap"
+### object. For reverse "AnnMap" objects, the mapping is "right-to-left".
+setClass("AnnMap",
+    representation(
+        "VIRTUAL",
+        join="character",
+        leftCol="character",
+        leftTable="character",
+        con="DBIConnection",
+        datacache="environment",
+        mapTarget="character", # "chip hgu95av2" or "YEAST" or...
+        mapName="character"
+    )
+)
+
+setClass("ReverseAnnMap", representation("VIRTUAL"))
+
+### For a "AtomicAnnMap" object, the right values are unnamed atomic vectors
+### (character or integer).
+### The last 2 slots ('replace.single' and 'replace.multiple') allow dealing
+### with silly maps ENTREZID and MULTIHIT in AGDB schema: they are complementary
+### maps that both map probeset ids to Entrez ids. In the ENTREZID map, probeset
+### ids that have multiple matches are mapped to "multiple". In the MULTIHIT
+### map, probeset ids that have <= 1 match are mapped to NAs. Sooo:
+###   - for ENTREZID: don't set replace.single (default is character(0)),
+###                   use replace.multiple="multiple",
+###   - for MULTIHIT: use replace.single=NA,
+###                   don't set replace.multiple (default is character(0)),
+###   - for any other map: don't set those fields (defaults will be just fine).
+setClass("AtomicAnnMap",
+    contains="AnnMap",
+    representation(
+        rightTable="character",
+        rightCol="character",
+        ## set only if the right names are tagged
+        tagsCol="character", 
+        ## set only if the right names need coercion after extraction
+        rightColType="character",
+        replace.single="character",
+        replace.multiple="character"
+    )
+)
+
+### DON'T ADD ANY SLOT HERE! A given AnnMap subclass and its corresponding
+### "reverse" class should always have exactly the same slots.
+setClass("ReverseAtomicAnnMap", contains=c("ReverseAnnMap", "AtomicAnnMap"))
+
+### For a "GOAnnMap" object, the right values are named lists of GO nodes,
+### each GO node being represented as a 3-element list of the form
+###   list(GOID="GO:0006470" , Evidence="IEA" , Ontology="BP")
+setClass("GOAnnMap",
+    contains="AnnMap",
+    representation(all="logical")
+)
+
+### Maps a GO term to a named character vector containing left values tagged
+### with the Evidence code.
+### DON'T ADD ANY SLOT HERE! (Why? See "ReverseAtomicAnnMap" def above.)
+setClass("ReverseGOAnnMap", contains=c("ReverseAnnMap", "GOAnnMap"))
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 setClass(
     "AnnDataPkgSeed",
     representation(
--- R/AnnMap-objects.R
+++ R/AnnMap-objects.R
@@ -6,9 +6,7 @@
 ###
 ### This file defines:
 ###
-###   a) The "AnnMap" class and subclasses.
-###
-###   b) A low-level API for the "AnnMap" objects:
+###   a) A low-level API for the "AnnMap" objects:
 ###        reverse
 ###        db
 ###        as.data.frame, nrow
@@ -27,7 +25,7 @@
 ###      For each of these methods, there are 2 "unoriented" methods: a left
 ###      method and a right method.
 ###
-###   c) Some helper functions used by this low-level API.
+###   b) Some helper functions used by this low-level API.
 ###
 ### The environment-like API for AnnMap objects (ls, mget, etc...) is defined
 ### in the AnnMap-envirAPI.R file.
@@ -210,76 +208,6 @@ GOtables <- function(all=FALSE)
     tables
 }
 
-
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### Classes representing SQLite-based annotation maps.
-###
-
-### An "AnnMap" object is a mapping between left values and right values.
-### The left values are the strings stored in the SQL col 'leftCol' of
-### table 'leftTable'.
-### For direct "AnnMap" objects, the mapping is "left-to-right". The left
-### values are the names (or symbols, or keys) of the map and are retrieved
-### with the "names" or "ls" methods. The type, format and location in the
-### DB of the right values depends on the particular subclass of the "AnnMap"
-### object. For reverse "AnnMap" objects, the mapping is "right-to-left".
-setClass("AnnMap",
-    representation(
-        "VIRTUAL",
-        join="character",
-        leftCol="character",
-        leftTable="character",
-        con="DBIConnection",
-        datacache="environment",
-        mapTarget="character", # "chip hgu95av2" or "YEAST" or...
-        mapName="character"
-    )
-)
-
-setClass("ReverseAnnMap", representation("VIRTUAL"))
-
-### For a "AtomicAnnMap" object, the right values are unnamed atomic vectors
-### (character or integer).
-### The last 2 slots ('replace.single' and 'replace.multiple') allow dealing
-### with silly maps ENTREZID and MULTIHIT in AGDB schema: they are complementary
-### maps that both map probeset ids to Entrez ids. In the ENTREZID map, probeset
-### ids that have multiple matches are mapped to "multiple". In the MULTIHIT
-### map, probeset ids that have <= 1 match are mapped to NAs. Sooo:
-###   - for ENTREZID: don't set replace.single (default is character(0)),
-###                   use replace.multiple="multiple",
-###   - for MULTIHIT: use replace.single=NA,
-###                   don't set replace.multiple (default is character(0)),
-###   - for any other map: don't set those fields (defaults will be just fine).
-setClass("AtomicAnnMap",
-    contains="AnnMap",
-    representation(
-        rightTable="character",
-        rightCol="character",
-        ## set only if the right names are tagged
-        tagsCol="character", 
-        ## set only if the right names need coercion after extraction
-        rightColType="character",
-        replace.single="character",
-        replace.multiple="character"
-    )
-)
-
-### DON'T ADD ANY SLOT HERE! A given AnnMap subclass and its corresponding
-### "reverse" class should always have exactly the same slots.
-setClass("ReverseAtomicAnnMap", contains=c("ReverseAnnMap", "AtomicAnnMap"))
-
-### For a "GOAnnMap" object, the right values are named lists of GO nodes,
-### each GO node being represented as a 3-element list of the form
-###   list(GOID="GO:0006470" , Evidence="IEA" , Ontology="BP")
-setClass("GOAnnMap",
-    contains="AnnMap",
-    representation(all="logical")
-)
-
-### Maps a GO term to a named character vector containing left values tagged
-### with the Evidence code.
-### DON'T ADD ANY SLOT HERE! (Why? See "ReverseAtomicAnnMap" def above.)
-setClass("ReverseGOAnnMap", contains=c("ReverseAnnMap", "GOAnnMap"))
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
