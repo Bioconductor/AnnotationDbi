4da872d48714d82fe5a91a1e521c7afea12f8e4c
Herve Pages <hpages@fhcrc.org>
2007-07-12 01:08:28 +0000

    Added support for the KEGG_DB schema. Version bump.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@25754 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/createAnnObjs.KEGG_DB.R
@@ -0,0 +1,84 @@
+### =========================================================================
+### An SQLite-based ann data package (AnnDbPkg) provides a set of pre-defined
+### AnnObj objects that are created at load-time. This set depends only on
+### the underlying db schema i.e. all the SQLite-based ann data packages that
+### share the same underlying db schema will provide the same set of AnnObj
+### objects.
+###
+### This file describes the set of AnnObj objects provided by any
+### KEGG_DB-based package i.e. any SQLite-based ann data package based
+### on the KEGG_DB schema.
+### The createAnnObjs.KEGG_DB() function is the main entry point for
+### this file: it is called by any KEGG_DB-based package at load-time.
+### -------------------------------------------------------------------------
+
+
+### Mandatory fields: objName, Class and L2Rpath
+KEGG_DB_AnnDbMap_seeds <- list(
+    list(
+        objName="PATHID2NAME",
+        Class="AtomicAnnDbMap",
+        L2Rpath=list(
+            list(
+                table="pathway2name",
+                Lcolname="path_id",
+                Rcolname="path_name"
+            )
+        )
+    ),
+    list(
+        objName="PATHID2EXTID",
+        Class="AtomicAnnDbMap",
+        L2Rpath=list(
+            list(
+                table="pathway2gene",
+                Lcolname="pathway_id",
+                Rcolname="gene_id"
+            )
+        )
+    ),
+    list(
+        objName="ENZYMEID2GO",
+        Class="AtomicAnnDbMap",
+        L2Rpath=list(
+            list(
+                table="ec2go",
+                Lcolname="ec_no",
+                Rcolname="go_id"
+            )
+        )
+    )
+)
+
+createAnnObjs.KEGG_DB <- function(prefix, objTarget, conn, datacache)
+{
+    ## AnnDbMap objects
+    seed0 <- list(
+        objTarget=objTarget,
+        datacache=datacache,
+        conn=conn
+    )
+    ann_objs <- createAnnDbMaps(KEGG_DB_AnnDbMap_seeds, seed0)
+
+    ## RevAtomicAnnDbMap objects
+    ann_objs$PATHNAME2ID <- revmap(ann_objs$PATHID2NAME, objName="PATHNAME2ID")
+    ann_objs$EXTID2PATHID <- revmap(ann_objs$PATHID2EXTID, objName="EXTID2PATHID")
+    ann_objs$GO2ENZYMEID <- revmap(ann_objs$ENZYMEID2GO, objName="GO2ENZYMEID")
+
+    ## 1 special map that is not an AnnDbMap object (just a named integer vector)
+    ann_objs$MAPCOUNTS <- createMAPCOUNTS(conn, prefix)
+
+    prefixAnnObjNames(ann_objs, prefix)
+}
+
+compareAnnDataIn2Pkgs.KEGG_DB <- function(pkgname1, pkgname2, prefix, quick=FALSE, verbose=FALSE)
+{
+    direct_maps <- sapply(KEGG_DB_AnnDbMap_seeds, function(x) x$objName)
+    reverse_maps <- c(
+        "PATHNAME2ID",
+        "EXTID2PATHID",
+        "GO2ENZYMEID"
+    )
+    compareAnnDataIn2Pkgs(pkgname1, pkgname2, prefix, direct_maps, reverse_maps, quick, verbose)
+}
+
