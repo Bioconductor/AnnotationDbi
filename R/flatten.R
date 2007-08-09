d0b656afe6ac05245449115122974d48e95e49bc
Herve Pages <hpages@fhcrc.org>
2007-08-09 17:26:40 +0000

    cosmetic
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@26255 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/flatten.R
+++ R/flatten.R
@@ -1,46 +1,53 @@
 ###
 ### Flattening different kinds of environments and maps
 ###
+### A toCV.<colname> function takes a vector (or a single NA) and returns
+### a character vector of the same length (or NULL).
+### A <colname>_CVL object is a list of character vector or NULL elements.
+###
 
 ### Flatten the GO envir found in HUMANCHIP and RODENTCHIP packages.
 flatten.GOenvir <- function(env)
 {
-    get.go_id <- function(golist)
+    ## 'x' is a named list of "GO triplets". The names are the GO ids.
+    ## A "GO triplet" is itself a list with 3 elements (GOID, Evidence,
+    ## Ontology).
+    toCV.go_id <- function(x)
     {
-        if (!is.list(golist)) {
-            if (!isTRUE(is.na(golist)))
+        if (!is.list(x)) {
+            if (!isTRUE(is.na(x)))
                 warning("right value in GO map is neither a list or an NA")
             return(NULL)
         }
-        names(golist)
+        names(x)
     }
-    get.Evidence <- function(golist)
+    toCV.Evidence <- function(x)
     {
-        if (!is.list(golist)) {
-            if (!isTRUE(is.na(golist)))
+        if (!is.list(x)) {
+            if (!isTRUE(is.na(x)))
                 warning("right value in GO map is neither a list or an NA")
             return(NULL)
         }
-        sapply(golist, function(goelt) goelt$Evidence, USE.NAMES=FALSE)
+        sapply(x, function(goelt) goelt$Evidence, USE.NAMES=FALSE)
     }
-    get.Ontology <- function(golist)
+    toCV.Ontology <- function(x)
     {
-        if (!is.list(golist)) {
-            if (!isTRUE(is.na(golist)))
+        if (!is.list(x)) {
+            if (!isTRUE(is.na(x)))
                 warning("right value in GO map is neither a list or an NA")
             return(NULL)
         }
-        sapply(golist, function(goelt) goelt$Ontology, USE.NAMES=FALSE)
+        sapply(x, function(goelt) goelt$Ontology, USE.NAMES=FALSE)
     }
-    go_id_map <- eapply(env, get.go_id, all.names=TRUE)
-    Evidence_map <- eapply(env, get.Evidence, all.names=TRUE)
-    Ontology_map <- eapply(env, get.Ontology, all.names=TRUE)
+    go_id_CVL <- eapply(env, toCV.go_id, all.names=TRUE)
+    Evidence_CVL <- eapply(env, toCV.Evidence, all.names=TRUE)
+    Ontology_CVL <- eapply(env, toCV.Ontology, all.names=TRUE)
 
-    lens <- sapply(go_id_map, length)    
-    probe_id <- rep(names(go_id_map), lens)
-    go_id <- unlist(go_id_map, recursive=FALSE, use.names=FALSE)
-    Evidence <- unlist(Evidence_map, recursive=FALSE, use.names=FALSE)
-    Ontology <- unlist(Ontology_map, recursive=FALSE, use.names=FALSE)
+    lens <- sapply(go_id_CVL, length)    
+    probe_id <- rep(names(go_id_CVL), lens)
+    go_id <- unlist(go_id_CVL, recursive=FALSE, use.names=FALSE)
+    Evidence <- unlist(Evidence_CVL, recursive=FALSE, use.names=FALSE)
+    Ontology <- unlist(Ontology_CVL, recursive=FALSE, use.names=FALSE)
    
     data.frame(probe_id=probe_id, Evidence=Evidence, go_id=go_id,
                Ontology=Ontology, stringsAsFactors=FALSE)
@@ -49,27 +56,28 @@ flatten.GOenvir <- function(env)
 ### Flatten the GO2PROBE envir found in HUMANCHIP and RODENTCHIP packages.
 flatten.GO2PROBEenvir <- function(env)
 {
-    ## 'probe_id' is a vector of probe ids
-    get.probe_id <- function(probe_id)
+    ## 'x' is a named character vector of probe ids. The names are the
+    ## Evidence codes.
+    toCV.probe_id <- function(x)
     {
-        if (any(is.na(probe_id)))
+        if (any(is.na(x)))
             warning("left value in GO2PROBE map contains NAs")
-        probe_id
+        x
     }
-    get.Evidence <- function(probe_id)
+    toCV.Evidence <- function(x)
     {
-        Evidence <- names(probe_id)
+        Evidence <- names(x)
         if (any(is.na(Evidence)))
             warning("left value in GO2PROBE map contains NAs")
         Evidence
     }
-    probe_id_map <- eapply(env, get.probe_id, all.names=TRUE)
-    Evidence_map <- eapply(env, get.Evidence, all.names=TRUE)
+    probe_id_CVL <- eapply(env, toCV.probe_id, all.names=TRUE)
+    Evidence_CVL <- eapply(env, toCV.Evidence, all.names=TRUE)
 
-    lens <- sapply(probe_id_map, length)
-    go_id <- rep(names(probe_id_map), lens)
-    probe_id <- unlist(probe_id_map, recursive=FALSE, use.names=FALSE)
-    Evidence <- unlist(Evidence_map, recursive=FALSE, use.names=FALSE)
+    lens <- sapply(probe_id_CVL, length)
+    go_id <- rep(names(probe_id_CVL), lens)
+    probe_id <- unlist(probe_id_CVL, recursive=FALSE, use.names=FALSE)
+    Evidence <- unlist(Evidence_CVL, recursive=FALSE, use.names=FALSE)
     
     data.frame(probe_id=probe_id, Evidence=Evidence, go_id=go_id,
                stringsAsFactors=FALSE)
