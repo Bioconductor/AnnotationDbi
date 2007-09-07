be8e64a1d9bdf087779c9494368668385fecb797
Marc Carlson <mcarlson@fhcrc.org>
2007-09-07 19:46:03 +0000

    Changes to support legacy version of YEAST.
    These changes are only here to just support the old YEAST package so
    that it can be "spoofed" to a classic format.
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@26942 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/createAnnObjs.YEASTLEGACY_DB.R
@@ -0,0 +1,214 @@
+### =========================================================================
+### An SQLite-based ann data package (AnnDbPkg) provides a set of pre-defined
+### AnnObj objects that are created at load-time. This set depends only on
+### the underlying db schema i.e. all the SQLite-based ann data packages that
+### share the same underlying db schema will provide the same set of AnnObj
+### objects.
+###
+### This file describes the set of AnnObj objects provided by any
+### YEASTLEGACY_DB-based package i.e. any SQLite-based ann data package based
+### on the YEASTLEGACY_DB schema.
+### The createAnnObjs.YEASTLEGACY_DB() function is the main entry point for
+### this file: it is called by any YEASTLEGACY_DB-based package at load-time.
+### -------------------------------------------------------------------------
+
+
+YEASTLEGACY_DB_L2Rlink1 <- list(tablename="sgd", Lcolname="systematic_name", Rcolname="id")
+
+### Mandatory fields: objName, Class and L2Rchain
+YEASTLEGACY_DB_AnnDbBimap_seeds <- list(
+    list(
+        objName="ALIAS",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="gene2alias",
+                Lcolname="id",
+                Rcolname="alias"
+            )
+        )
+    ),
+    list(
+        objName="CHR",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="chromosome_features",
+                Lcolname="id",
+                Rcolname="chromosome"
+            )
+        )
+    ),
+    list(
+        objName="CHRLOC",
+        Class="AnnDbMap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="chromosome_features",
+                Lcolname="id",
+                tagname=c(Chromosome="{chromosome}"),
+                Rcolname="start"
+            )
+        ),
+        rightColType="integer"
+    ),
+    list(
+        objName="DESCRIPTION",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="chromosome_features",
+                Lcolname="id",
+                Rcolname="feature_description"
+            )
+        )
+    ),
+    list(
+        objName="ENZYME",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="ec",
+                Lcolname="id",
+                Rcolname="ec_number"
+            )
+        )
+    ),
+    list(
+        objName="GENENAME",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            list(
+                tablename="sgd",
+                Lcolname="systematic_name",
+                Rcolname="gene_name"
+            )
+        )
+    ),
+    list(
+        objName="PATH",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="kegg",
+                Lcolname="id",
+                Rcolname="kegg_id"
+            )
+        )
+    ),
+    list(
+        objName="PMID",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="pubmed",
+                Lcolname="id",
+                Rcolname="pubmed_id"
+            )
+        )
+    ),
+    list(
+        objName="GO",
+        Class="Go3AnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                #tablename="go_term", # no rightmost table for a Go3AnnDbBimap
+                Lcolname="id",
+                tagname=c(Evidence="{evidence}"),
+                Rcolname="go_id",
+                Rattribnames=c(Ontology="NULL")
+            )
+        ),
+        rightTables=Go3tablenames()
+    ),
+    list(
+        objName="COMMON2SYSTEMATIC",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            list(
+                tablename="gene2systematic",
+                Lcolname="gene_name",
+                Rcolname="systematic_name"
+            )
+        )
+    ),
+    list(
+        objName="INTERPRO",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="interpro",
+                Lcolname="id",
+                Rcolname="interpro_id"
+            )
+        )
+    ),
+    list(
+        objName="PFAM",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="pfam",
+                Lcolname="id",
+                Rcolname="pfam_id"
+            )
+        )
+    ),
+    list(
+        objName="SMART",
+        Class="AtomicAnnDbBimap",
+        L2Rchain=list(
+            YEASTLEGACY_DB_L2Rlink1,
+            list(
+                tablename="smart",
+                Lcolname="id",
+                Rcolname="smart_id"
+            )
+        )
+    )
+)
+
+createAnnObjs.YEASTLEGACY_DB <- function(prefix, objTarget, conn, datacache)
+{
+    ## AnnDbBimap objects
+    seed0 <- list(
+        objTarget=objTarget,
+        datacache=datacache,
+        conn=conn
+    )
+    ann_objs <- createAnnDbBimaps(YEASTLEGACY_DB_AnnDbBimap_seeds, seed0)
+
+    ## Reverse maps
+    ann_objs$ENZYME2PROBE <- revmap(ann_objs$ENZYME, objName="ENZYME2PROBE")
+    ann_objs$PATH2PROBE <- revmap(ann_objs$PATH, objName="PATH2PROBE")
+    ann_objs$PMID2PROBE <- revmap(ann_objs$PMID, objName="PMID2PROBE")
+    ann_objs$GO2PROBE <- revmap(ann_objs$GO, objName="GO2PROBE")
+
+    ## 3 special maps that are not AnnDbBimap objects (just named vectors)
+    ann_objs$CHRLENGTHS <- createCHRLENGTHS(conn)
+    ann_objs$REJECTORF <- createREJECTORF(conn)
+    ann_objs$MAPCOUNTS <- createMAPCOUNTS(conn, prefix)
+
+    ## extra maps for generating env-based package
+    map <- ann_objs$GO2PROBE
+    map@rightTables <- Go3tablenames(all=TRUE)
+    map@objName <- "GO2ALLPROBES"
+    ann_objs$GO2ALLPROBES <- map
+
+
+    ## Some pre-caching
+    Lkeys(ann_objs$GO)
+
+    prefixAnnObjNames(ann_objs, prefix)
+}
+
