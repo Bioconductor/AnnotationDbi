fee9b27e29d3ffd87aa75c5b8eadbb7ef79da070
Marc Carlson <mcarlson@fhcrc.org>
2008-04-15 02:33:43 +0000

    Fixed bad but subtle bug in SQLforge.
    This fixes a bad bug in SQLforge that would under certain rare
    circumstances, map the wrong type of indentifier into the entrez gene
    field.  Now the code does a better job of making sure that when it has
    an alternate ID, to find a correct entrez gene id if one can be found
    and stick that into the entrez gene field.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@31080 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/sqlForge_baseMapBuilder.R
+++ R/sqlForge_baseMapBuilder.R
@@ -78,7 +78,7 @@ probe2gene <- function(baseMap, otherSrc,
         #If there might be refseq IDs, then they might have unwanted .<digit> extensions which must be removed.
         #This (unfortunately cannot be done for the otherSrc files since I won't know what I am getting in that case).
         if(baseMapType=='refseq' || baseMapType=='gbNRef'){  #need to verify that this function is not destructive to genbank IDs
-#        if(baseMapType=='refseq'){
+        #if(baseMapType=='refseq'){
             baseMap=cleanRefSeqs(baseMap)
         }
         
@@ -96,12 +96,14 @@ probe2gene <- function(baseMap, otherSrc,
 	attach_sql <- paste("ATTACH DATABASE '",chipMapSrc,"' AS src;", sep="", collapse="")
 	sqliteQuickSQL(db, attach_sql)
 	if(baseMapType=='eg') {
+                message(cat("baseMapType is eg"))
 		sqliteQuickSQL(db, "INSERT INTO probe2acc SELECT probe_id, NULL FROM probes_ori;")
 		sql <- paste("INSERT INTO probe2gene",
 			   "SELECT DISTINCT probe_id, gene_id",
 			    "FROM curr_map GROUP BY probe_id;", sep=" ", collapse="")
 		sqliteQuickSQL(db, sql)
 	} else if (baseMapType=='ug') {
+                message(cat("baseMapType is ug"))
 		sqliteQuickSQL(db, "INSERT INTO probe2acc SELECT probe_id, NULL FROM probes_ori;")
 		sql <- paste("INSERT INTO probe2gene",
 			    "SELECT DISTINCT c.probe_id, u.gene_id",
@@ -109,6 +111,7 @@ probe2gene <- function(baseMap, otherSrc,
 			    "WHERE c.gene_id=u.unigene_id GROUP BY c.probe_id;", sep=" ", collapse="")
 		sqliteQuickSQL(db, sql)
 	} else if (baseMapType=='refseq') {
+                message(cat("baseMapType is refseq"))
 		sqliteQuickSQL(db, "CREATE INDEX cm1 ON curr_map(probe_id);")
 		sqliteQuickSQL(db, "INSERT INTO probe2acc SELECT p.probe_id, c.gene_id FROM probes_ori AS p LEFT OUTER JOIN curr_map AS c ON p.probe_id=c.probe_id GROUP BY p.probe_id;")
 		sql <- paste("INSERT INTO probe2gene", 
@@ -117,6 +120,7 @@ probe2gene <- function(baseMap, otherSrc,
 			    "WHERE c.gene_id=a.accession GROUP BY c.probe_id;", sep=" ", collapse="") 
 		sqliteQuickSQL(db, sql)
 	} else 	{ ### type='gb' or 'gbNRef'
+                message(cat("baseMapType is gb or gbNRef"))
 		sqliteQuickSQL(db, "CREATE INDEX cm1 ON curr_map(probe_id);")
 		sqliteQuickSQL(db, "INSERT INTO probe2acc SELECT p.probe_id, c.gene_id FROM probes_ori AS p LEFT OUTER JOIN curr_map AS c ON p.probe_id=c.probe_id GROUP BY p.probe_id;")
 		sql <- paste("INSERT INTO probe2gene", 
@@ -126,11 +130,11 @@ probe2gene <- function(baseMap, otherSrc,
 		sqliteQuickSQL(db, sql)
 		sql <- paste("DELETE FROM curr_map WHERE probe_id IN",
 			"(SELECT probe_id FROM probe2gene);", sep=" ", collapse="")
-		sqliteQuickSQL(db, sql)
+ 		sqliteQuickSQL(db, sql)
 		sql <- paste("INSERT INTO probe2gene ", 
 			    "SELECT DISTINCT c.probe_id, a.gene_id ",
-			    "FROM curr_map as c, src.accession_unigene as a",
-			    "WHERE c.gene_id=a.accession GROUP BY c.probe_id;", sep=" ", collapse="") 
+			    "FROM curr_map as c, src.accession_unigene as a",  #The HORRIBLY MISNAMED accession_unigene table contains refseq IDs as well as GenBank IDs but no unigene IDs!
+                            "WHERE c.gene_id=a.accession GROUP BY c.probe_id;", sep=" ", collapse="") #This name is not my fault!  NOT MY FAULT! - Marc
 		sqliteQuickSQL(db, sql)
 	}
 	#sqliteQuickSQL(db, "DETACH DATABASE src;")
@@ -142,7 +146,7 @@ probe2gene <- function(baseMap, otherSrc,
         #of these packages (that contain all the refseqs and genbanks ever
         #matched to an entrez ID)
 	if (allowHomologyGene && baseMapType %in% c('refseq', 'gb', 'gbNRef')) {
-                cat("Searching for other useful ID's from the \"all\" indices \n")
+                message(cat("Searching for other useful ID's from the \"all\" indices \n"))
 		#sqliteQuickSQL(db, paste("ATTACH DATABASE '",chipmapsrc,"' AS src;",sep=""))
 		sqliteQuickSQL(db,
 			"DELETE FROM curr_map WHERE probe_id IN (SELECT probe_id FROM probe2gene);")
@@ -179,8 +183,40 @@ probe2gene <- function(baseMap, otherSrc,
 		"CREATE TEMP TABLE other_stat AS SELECT probe_id, gene_id, count(*) as vote FROM other GROUP BY probe_id, gene_id;")
 	sqliteQuickSQL(db,
 		"INSERT INTO other_rank (probe_id, gene_id, vote, row_id) SELECT DISTINCT o.probe_id AS Probe, o.gene_id AS Gene, s.vote AS Vote, o.rowid AS Row FROM other AS o, other_stat AS s WHERE o.probe_id=s.probe_id AND o.gene_id=s.gene_id ORDER BY Probe asc, Vote desc, Row asc;")
-	sqliteQuickSQL(db,
-		"INSERT INTO probe2gene SELECT DISTINCT probe_id, gene_id FROM other_rank WHERE rowid IN (SELECT min(rowid) FROM other_rank GROUP BY probe_id);")
+
+        ##The following worked great when it was always affy data (and the extra data in the "other_rank" was always an EG)
+        ##but now we have stuff like unigene IDs that can be in these fields, so we need to search the DB, and get out any EGs that match from the chipmapsrc and
+        ##put THOSE into the probe2-gene mapping.
+
+        ##NOW I want to use the stuff inside of "other_rank" directly if it is already *IN* the src.EGList, but if it is not, then we want to do another search of the 3 fields: accession_unigene, refseq and accession
+
+        ##We will be needing the chipMapSrc file once again...
+ 	attach_sql <- paste("ATTACH DATABASE '",chipMapSrc,"' AS src;", sep="", collapse="")
+ 	sqliteQuickSQL(db, attach_sql)
+
+        ##The following will insert any entrez gene IDs into the database.
+        sql <- "INSERT INTO probe2gene SELECT DISTINCT probe_id, gene_id FROM other_rank WHERE rowid IN (SELECT min(rowid) FROM other_rank GROUP BY probe_id)
+                 AND gene_id IN src.EGList;"
+ 	sqliteQuickSQL(db, sql)
+
+        ##TODO?  Someone might want it set up so that if their unigene etc. is not found, it is still jammed in there as a unigene.  Right now we ignore it if we don't have an EG...
+
+        ##Now to proceed I need another tabled to hold only the minimal row number information from the other_rank table
+        sqliteQuickSQL(db, "CREATE TABLE min_other_rank (probe_id TEXT, gene_id TEXT, vote INTEGER,  row_id INTEGER);")
+        sqliteQuickSQL(db, "INSERT INTO min_other_rank SELECT * FROM other_rank WHERE rowid IN (SELECT min(rowid) FROM other_rank GROUP BY probe_id);")
+        
+        ##The following will insert any unigene IDs into the database AS entrez gene IDs, (by joining with the prefiltered min_other_rank table)
+        sql <- "INSERT INTO probe2gene SELECT DISTINCT m.probe_id, u.gene_id FROM min_other_rank as m INNER JOIN src.unigene as u WHERE m.gene_id=u.unigene_id;"
+ 	sqliteQuickSQL(db, sql)
+
+        ##The following will insert any missing refseq IDs into the database AS entrez gene IDs, (by joining with the prefiltered min_other_rank table)
+        sql <- "INSERT INTO probe2gene SELECT DISTINCT m.probe_id, r.gene_id FROM min_other_rank as m INNER JOIN src.refseq as r WHERE m.gene_id=r.accession;"
+ 	sqliteQuickSQL(db, sql)
+        
+        ##The following will insert any missing GenBank IDs into the database AS entrez gene IDs, (by joining with the prefiltered min_other_rank table)
+        sql <- "INSERT INTO probe2gene SELECT DISTINCT m.probe_id, gb.gene_id FROM min_other_rank as m INNER JOIN src.accession as gb WHERE m.gene_id=gb.accession;"
+ 	sqliteQuickSQL(db, sql)
+        
 	sqliteQuickSQL(db, "INSERT INTO probe2gene SELECT probe_id, NULL FROM probes_ori WHERE probe_id NOT IN (SELECT probe_id FROM probe2gene);")
 	sqliteQuickSQL(db, "CREATE INDEX p1 ON probe2gene(probe_id);")
 	sqliteQuickSQL(db, "INSERT INTO probe_map SELECT DISTINCT p.probe_id, p.gene_id, a.gene_id FROM probe2acc as a LEFT OUTER JOIN probe2gene as p ON a.probe_id=p.probe_id;")
@@ -189,6 +225,7 @@ probe2gene <- function(baseMap, otherSrc,
         sqliteQuickSQL(db, "DROP TABLE probe2gene;")
         sqliteQuickSQL(db, "DROP TABLE probe2acc;")
         sqliteQuickSQL(db, "DROP TABLE probes_ori;")
+        sqliteQuickSQL(db, "DROP TABLE min_other_rank;")
         dbDisconnect(db)
 	pkgName
 }
