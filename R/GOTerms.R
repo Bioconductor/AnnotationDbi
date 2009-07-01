cd9c9daf259cff9169fcc2c5c05735f697509c16
Marc Carlson <mcarlson@fhcrc.org>
2009-07-01 01:58:59 +0000

    Overloads the Term and related methods for GOTERM
    This update overloads the Term, Ontology etc. methods for GOTERM so
    that they can take IDs or GOTermsAnnDbBimap.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@40437 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GOTerms.R
+++ R/GOTerms.R
@@ -78,6 +78,62 @@ setMethod("Synonym", "GOTerms", function(object) object@Synonym)
 setMethod("Secondary", "GOTerms", function(object) object@Secondary)
 
 
+##.GOid2go_termField() retrieves ids of type field from go_term
+.GOid2go_termField <- function(ids, field){
+    require("GO.db")
+##     message(cat("Before SQL \n")) ##test
+    sql <- sprintf("SELECT go_id, %s
+                    FROM go_term
+                    WHERE go_id IN ('%s')",
+                   field,
+                   paste(ids, collapse="','"))
+    res <- dbGetQuery(GO_dbconn(), sql)
+    if(dim(res)[1]==0 && dim(res)[2]==0){
+        stop("None of your IDs match IDs from GO.  Are you sure you have valid IDs?")
+    }else{
+        ans <- res[[2]]
+        names(ans) <- res[[1]]
+        return(ans[ids]) ##This only works because each GO ID is unique (and therefore a decent index ID)
+    }
+}
+
+setMethod("GOID", "GOTermsAnnDbBimap",function(object) .GOid2go_termField(keys(object),"go_id") )
+setMethod("GOID", "character",function(object) .GOid2go_termField(object,"go_id") )
+
+setMethod("Term", "GOTermsAnnDbBimap",function(object) .GOid2go_termField(keys(object),"term") )
+setMethod("Term", "character",function(object) .GOid2go_termField(object,"term") )
+
+setMethod("Ontology", "GOTermsAnnDbBimap",function(object) .GOid2go_termField(keys(object),"ontology") )
+setMethod("Ontology", "character",function(object) .GOid2go_termField(object,"ontology") )
+
+setMethod("Definition", "GOTermsAnnDbBimap",function(object) .GOid2go_termField(keys(object),"definition") )
+setMethod("Definition", "character",function(object) .GOid2go_termField(object,"definition") )
+
+
+##.GOid2go_synonymField() retrieves ids of type field from go_synonym
+.GOid2go_synonymField <- function(ids, field){
+    require("GO.db")
+    sql <- paste("SELECT gt.go_id, gs.",field,"
+                  FROM go_term AS gt, go_synonym AS gs
+                  WHERE gt._id=gs._id AND go_id IN ('",paste(ids, collapse="','"),"')", sep="")
+    res <- dbGetQuery(GO_dbconn(), sql)
+    if(dim(res)[1]==0 && dim(res)[2]==0){
+        stop("None of your IDs match IDs from GO.  Are you sure you have valid IDs?")
+    }else{
+        ans = split(res[,2],res[,1])
+        return(ans[ids])##once again (this time in list context), we are indexing with unique IDs.
+    }
+}
+
+setMethod("Synonym", "GOTermsAnnDbBimap",function(object) .GOid2go_synonymField(keys(object),"synonym") )
+setMethod("Synonym", "character",function(object) .GOid2go_synonymField(object,"synonym") )
+
+setMethod("Secondary", "GOTermsAnnDbBimap",function(object) .GOid2go_synonymField(keys(object),"secondary") )
+setMethod("Secondary", "character",function(object) .GOid2go_synonymField(object,"secondary") )
+
+
+
+
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### The "show" methods.
 ###
