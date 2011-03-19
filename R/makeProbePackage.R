8534284c02100b2952f8bc30bd2464d625ef2fc9
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2011-03-19 18:36:40 +0000

    I niced up the print method for "probetable" objects.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@53900 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/makeProbePackage.R
+++ R/makeProbePackage.R
@@ -1,5 +1,9 @@
-print.probetable = function(x, ...) {
-  cat("Object of class", class(x), "with", nrow(x), "rows and", ncol(x), "columns.\n")
+print.probetable = function(x, maxrows = 3, ...) {
+  nr = nrow(x)
+  np = min(nr, maxrows)
+  cat("Object of class", class(x), "with", nr, "rows and", ncol(x), "columns.\n")
+  if(nr>maxrows) cat("First", np, "rows are:\n")
+  print.data.frame(x[seq_len(np), ])
 }
 
 
