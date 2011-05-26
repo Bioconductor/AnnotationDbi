0f821580abf0562e829993f557bc2b0161631a09
Marc Carlson <mcarlson@fhcrc.org>
2011-05-26 16:53:54 +0000

    Adds support for blast2GO to org generator
    This became necessary since many of NCBIs organisms do not have GO to
    gene mappings.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@55840 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/NCBI_ftp.R
+++ R/NCBI_ftp.R
@@ -1,134 +1,3 @@
-## list requirements
-require(RSQLite)
-
-
-## For now, I am just going to use what I know works 
-## (even though depending on curl hides a dependency which is BAD umkay):
-## But the following chunck should at least download a file and return a 
-## (cleaned) data.frame from it
-.downloadData <- function (file, tax_id) {
-  ## file is something like: "gene2go.gz"
-  message(paste("Getting data for ",names(file),sep=""))
-## The following works, but commented so that I can avoid the wrath of NCBI
-#   url <- paste("ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/",names(file), sep="")
-#   tmp <- tempfile()
-#   download.file(url, tmp, method="curl", quiet=TRUE)
-#   vals <- read.delim(url, header=FALSE, sep="\t", skip=1, stringsAsFactors=FALSE, colClasses = colClasses)
-  
-## TEMPORARILY, lets work from some local files.
-  ## Lets also speed this part up by declaring the column types, and
-  ## subsetting out the things that don't match our tax ID.
-  colClasses <- c(rep("character", times= length(unlist(file))))
-  url <- paste("/home/mcarlson/proj/mcarlson/",
-               "2011-May24/",names(file), sep="")
-  vals <- read.delim(url, header=FALSE, sep="\t", skip=1, stringsAsFactors=FALSE, colClasses = colClasses)
-
-##   
-  ## The following will just keep unwanted data from our temp DB tables.
-  ## if there is a tax_id,
-  ## then only return the piece that matches the organism in question
-  colnames(vals) <- unlist(file)
-  if(!is.null(vals[["tax_id"]])){
-    vals <- vals[vals[["tax_id"]]==tax_id,]
-  }
-  vals
-}
-
-
-## need to be able to generate the columns data from the files and the types.
-.generateCols <- function (file) {
-  types <- c("tax_id" = "INTEGER NOT NULL",
-             "gene_id" = "INTEGER NOT NULL",
-             "go_id" = "TEXT NOT NULL",
-             "evidence" = "TEXT",
-             "go_qualifier" = "TEXT",
-             "go_description" = "TEXT",
-             "pubmed_id" = "INTEGER",
-             "category" = "TEXT",
-             "status" = "TEXT",
-             "rna_accession" = "TEXT",
-             "rna_gi" = "INTEGER",
-             "protein_accession" = "TEXT",
-             "protein_gi" = "INTEGER",
-             "genomic_dna_accession" = "TEXT",
-             "genomic_dna_gi" = "INTEGER",
-             "genomic_start" = "INTEGER",
-             "genomic_end" = "INTEGER",
-             "orientation" = "TEXT",
-             "assembly" = "TEXT",
-             "unigene_id" = "TEXT",
-             "symbol" = "TEXT",
-             "locus_tag" = "TEXT",
-             "synonyms" = "TEXT",
-             "dbXrefs" = "TEXT",
-             "chromosome" = "TEXT",
-             "map_location" = "TEXT",
-             "description" = "TEXT",
-             "gene_type" = "TEXT",
-             "nomenclature_symbol" = "TEXT",
-             "nomenclature_name" = "TEXT",
-             "nomenclature_status" = "TEXT",
-             "other_designations" = "TEXT",
-             "mim_id" = "INTEGER NOT NULL",
-             "relation_type" = "TEXT",
-             "refseq_id" = "TEXT NOT NULL",
-             "uniprot_id" = "TEXT NOT NULL"
-             )
-  cols <- character()
-  for(i in seq_len(length(file[[1]]))){
-    cols[i] <-  paste(file[[1]][i],types[file[[1]]][i])
-  }
-  paste(cols, collapse =",")
-}
-
-
-## also need somethign to generate the insert statement.
-.generateINSERTStatement <- function (file) {
-  Qs <- paste(rep("?",length(file[[1]])),  collapse=",")
-  paste("INSERT INTO ",sub(".gz$","",names(file)),
-  " VALUES(",Qs,");",sep="")
-}
-
-## need code to generate a table for each of the
-## file below is like: files[1] or files[2] 
-.createTEMPTable <- function (con, file, tax_id) {
-  data <- .downloadData(file, tax_id)
-  table <- sub(".gz$","",names(file))
-  if(is.null(table)) stop("Unable to infer a table name.")
-  cols <- .generateCols(file)
-  message(paste("Populating ",table," table:", sep=""))
-  sql<- paste("CREATE TABLE IF NOT EXISTS ",table," (",cols,");")
-  # example cols:
-  #     _id INTEGER PRIMARY KEY,
-  #     gene_id VARCHAR(10) NOT NULL UNIQUE           -- Entrez Gene ID
-  sqliteQuickSQL(con, sql)
-  
-  
-  #data <- data.frame(data) ## TODO: data.frame() necessary???
-  sql <- .generateINSERTStatement(file)
-  # sql<- paste("INSERT INTO ",table,"(data) VALUES(?);",sep="")
-  dbBeginTransaction(con)
-  dbGetPreparedQuery(con, sql, data)
-  dbCommit(con)
-  message(paste("table ",table," filled",sep=""))
-}
-
-
-## loop through and make the tables
-.makeBaseDBFromDLs <- function(files, tax_id, con){
-  for(i in seq_len(length(files))){
-    .createTEMPTable(con, files[i], tax_id)
-  }
-  con
-}
-
-.dropOldTables <- function(fileNames){
-  fileNames <- sub(".gz", "", fileNames)
-  for(i in seq_len(length(fileNames))){
-    sqliteQuickSQL(con, paste("DROP TABLE ",fileNames[i],sep=""))
-  }
-}
-
 #########################################################################
 #########################################################################
 ####         Helper functions for generating a generic DB            ####
@@ -272,47 +141,282 @@ require(RSQLite)
 }
 
 
+#########################################################################
+#########################################################################
+
+## For now, I am just going to use what I know works 
+## (even though depending on curl hides a dependency which is BAD umkay):
+## But the following chunck should at least download a file and return a 
+## (cleaned) data.frame from it
+.downloadData <- function (file, tax_id) {
+  colClasses <- c(rep("character", times= length(unlist(file))))
+  ## names(file) is something like: "gene2go.gz"
+  message(paste("Getting data for ",names(file),sep=""))
+## The following works, but commented so that I can avoid the wrath of NCBI
+#   url <- paste("ftp://ftp.ncbi.nlm.nih.gov/gene/DATA/",names(file), sep="")
+#   tmp <- tempfile()
+#   download.file(url, tmp, method="curl", quiet=TRUE)
+#   vals <- read.delim(tmp, header=FALSE, sep="\t", skip=1, stringsAsFactors=FALSE, colClasses = colClasses)
+  
+## TEMPORARILY, lets work from some local files.
+  ## Lets also speed this part up by declaring the column types, and
+  ## subsetting out the things that don't match our tax ID.
+  url <- paste("/home/mcarlson/proj/mcarlson/",
+               "2011-May24/",names(file), sep="")
+  vals <- read.delim(url, header=FALSE, sep="\t", skip=1, stringsAsFactors=FALSE, colClasses = colClasses)
+
+##   
+  ## The following will just keep unwanted data from our temp DB tables.
+  ## if there is a tax_id,
+  ## then only return the piece that matches the organism in question
+  colnames(vals) <- unlist(file)
+  if(!is.null(vals[["tax_id"]])){
+    vals <- vals[vals[["tax_id"]]==tax_id,]
+  }
+  vals
+}
+
+## helper to populate base tables
+.populateBaseTable <- function(con, sql, data, table) {
+    dbBeginTransaction(con)
+    dbGetPreparedQuery(con, sql, data)
+    dbCommit(con)
+    message(paste("table ",table," filled",sep=""))
+}
+
+## Convenience function for quickly making indices
+.makeIndex <- function(con,table, field){
+  indName <-paste("ind",table,field,sep="_")
+  sqliteQuickSQL(con,
+   paste("CREATE INDEX IF NOT EXISTS",indName,"ON",table,"(",field,")"))
+}
+
+## Use when there is no GO data
+.getBlast2GOData <- function(tax_id, con) {
+  url = paste("http://bioinfo.cipf.es/b2gfar/_media/species:data:",
+        tax_id,".annot.zip",sep="")
+  tmp <- tempfile()
+  download.file(url, tmp, method="curl", quiet=TRUE)
+  vals <- read.delim(unzip(tmp), header=FALSE, sep="\t",
+                     stringsAsFactors=FALSE)
+  ## I will need to so extra stuff here to match up categories etc.
+  ## (vals has to look like gene2go would, and I have to join to refseq and to
+  ## accession just to get my EGs)
+  RSVals <- vals[grep("refseq",vals[,4]),c(2,3)]
+  GBVals <- vals[grep("genbank",vals[,4]),c(2,6)]
+  colnames(GBVals) <- colnames(RSVals) <- c("go_id","accession")
+  tables <- sqliteQuickSQL(con,"SELECT * FROM sqlite_master")$name
+  
+  if("gene2refseq" %in% tables){
+    g2rs <- sqliteQuickSQL(con,
+      "SELECT gene_id, protein_accession FROM gene2refseq")
+    g2rs[,2] <- sub("\\.\\d+$","",g2rs[,2])
+    vals <- merge(RSVals,g2rs, by.x="accession" , by.y="protein_accession")
+  }else if("gene2accession" %in% tables){
+    g2ac <- sqliteQuickSQL(con,
+      "SELECT gene_id, protein_accession FROM gene2accession")
+    gb <- merge(GBVals,g2ac, by.x="accession" , by.y="protein_accession")
+    if("gene2refseq" %in% tables){
+      vals <- rbind(vals, gb)
+    }else{
+      vals <- gb
+    }
+  }else{
+    stop("It is impossible to match up blasted GO terms with NCBI accessions.")
+  }
+  
+  
+  ## 1st attempt (check in, then delete it)
+  ## if there is accession data, then we want to join to it.
+  ## tables <- sqliteQuickSQL(con,"SELECT * FROM sqlite_master")$name
+  ## if("gene2refseq" %in% tables){
+  ##   sqliteQuickSQL(con,
+  ##    "CREATE TABLE rsB2GO(go_id VARCHAR(10) NOT NULL, accession VARCHAR(10))")
+  ##   sql <- "INSERT INTO rsB2GO(go_id, accession) VALUES(?,?);"
+  ##   .populateBaseTable(con, sql, RSVals, "rsB2GO")
+  ##   ## Too slow so lets make some indices (temp)
+  ##   .makeIndex(con, "rsB2GO", "accession")
+  ##   .makeIndex(con, "gene2refseq", "protein_accession")
+  ##   ## then join those to the other tables to get the data we need.
+  ##   ## for RS join to "protein_accession"
+  ##   rs <- sqliteQuickSQL(con,
+  ##    paste("SELECT g.go_id, a.gene_id FROM rsB2GO AS g, gene2refseq AS a",
+  ##          "WHERE g.accession = a.protein_accession"))
+  ## }else if("gene2accession" %in% tables){
+  ##   sqliteQuickSQL(con,
+  ##    "CREATE TABLE gbB2GO(go_id VARCHAR(10) NOT NULL, accession VARCHAR(10))")
+  ##   sql <- "INSERT INTO gbB2GO(go_id, accession) VALUES(?,?);"
+  ##   .populateBaseTable(con, sql, GBVals, "gbB2GO")
+  ##   .makeIndex(con, "gbB2GO", "accession")
+  ##   .makeIndex(con, "gene2accession", "protein_accession")
+  ##   ## for GB join to "protein_accession"
+  ##   gb <- sqliteQuickSQL(con,
+  ##    paste("SELECT g.go_id, a.gene_id FROM gbB2GO AS g, gene2accession AS a",
+  ##          "WHERE g.accession = a.protein_accession"))
+  ## }
+  ## end of 1st attempt
+  
+  ## then assemble the data back together to make it look like gene2go so
+  ## it can be inserted there.
+  rlen <- dim(vals)[1]
+  data <-data.frame(tax_id = rep(tax_id, rlen),
+                   gene_id = as.character(vals[["gene_id"]]),
+                   go_id = vals[["go_id"]],
+                   evidence = rep("IEA", rlen),
+                   go_qualifier = rep("-", rlen),
+                   go_description = rep("-", rlen),
+                   pubmed_id = rep("-", rlen),
+                   category = Ontology(vals[["go_id"]]),
+                   stringsAsFactors=FALSE)
+  data[,8] <- sub("BP","Process",data[,8])
+  data[,8] <- sub("CC","Component",data[,8])
+  data[,8] <- sub("MF","Function",data[,8])
+  data
+}
+
+## need to be able to generate the columns data from the files and the types.
+.generateCols <- function (file) {
+  types <- c("tax_id" = "INTEGER NOT NULL",
+             "gene_id" = "INTEGER NOT NULL",
+             "go_id" = "TEXT NOT NULL",
+             "evidence" = "TEXT",
+             "go_qualifier" = "TEXT",
+             "go_description" = "TEXT",
+             "pubmed_id" = "INTEGER",
+             "category" = "TEXT",
+             "status" = "TEXT",
+             "rna_accession" = "TEXT",
+             "rna_gi" = "INTEGER",
+             "protein_accession" = "TEXT",
+             "protein_gi" = "INTEGER",
+             "genomic_dna_accession" = "TEXT",
+             "genomic_dna_gi" = "INTEGER",
+             "genomic_start" = "INTEGER",
+             "genomic_end" = "INTEGER",
+             "orientation" = "TEXT",
+             "assembly" = "TEXT",
+             "unigene_id" = "TEXT",
+             "symbol" = "TEXT",
+             "locus_tag" = "TEXT",
+             "synonyms" = "TEXT",
+             "dbXrefs" = "TEXT",
+             "chromosome" = "TEXT",
+             "map_location" = "TEXT",
+             "description" = "TEXT",
+             "gene_type" = "TEXT",
+             "nomenclature_symbol" = "TEXT",
+             "nomenclature_name" = "TEXT",
+             "nomenclature_status" = "TEXT",
+             "other_designations" = "TEXT",
+             "mim_id" = "INTEGER NOT NULL",
+             "relation_type" = "TEXT",
+             "refseq_id" = "TEXT NOT NULL",
+             "uniprot_id" = "TEXT NOT NULL"
+             )
+  cols <- character()
+  for(i in seq_len(length(file[[1]]))){
+    cols[i] <-  paste(file[[1]][i],types[file[[1]]][i])
+  }
+  paste(cols, collapse =",")
+}
+
+
+## also need somethign to generate the insert statement.
+.generateTempTableINSERTStatement <- function (file) {
+  Qs <- paste(rep("?",length(file[[1]])),  collapse=",")
+  paste("INSERT INTO ",sub(".gz$","",names(file)),
+  " VALUES(",Qs,");",sep="")
+}
+
+
+## need code to generate a table for each of the
+## file below is like: files[1] or files[2]
+.createTEMPNCBIBaseTable <- function (con, file, tax_id) {
+  data <- .downloadData(file, tax_id)
+  table <- sub(".gz$","",names(file))
+  if(is.null(table)) stop("Unable to infer a table name.")
+  cols <- .generateCols(file)
+  sql<- paste("CREATE TABLE IF NOT EXISTS ",table," (",cols,");")
+  sqliteQuickSQL(con, sql)
+  
+  message(paste("Populating ",table," table:", sep=""))
+  sql <- .generateTempTableINSERTStatement(file)
+  if(dim(data)[1] != 0){ ## ie. there has to be SOME data...
+    .populateBaseTable(con, sql, data, table)
+  }else if(dim(data)[1] == 0 && file=="gene2go"){ ## we need blast2GO
+    message(paste("Getting blast2GO data as a substitute for ",table,sep=""))
+    data <- .getBlast2GOData(tax_id, con)
+    sql <- paste("INSERT INTO gene2go(tax_id, gene_id, go_id, evidence, go_qualifier, go_description,",
+                 "pubmed_id, category) VALUES(?,?,?,?,?,?,?,?);")
+    .populateBaseTable(con, sql, data, table)
+  }else{
+    message(paste("No data available for table ",table,sep=""))
+  }
+}
+
+
+## loop through and make the tables
+.makeBaseDBFromDLs <- function(files, tax_id, con){
+  for(i in seq_len(length(files))){
+    .createTEMPNCBIBaseTable(con, files[i], tax_id)
+  }
+  con
+}
+
+
+.dropOldTables <- function(fileNames){
+  fileNames <- sub(".gz", "", fileNames)
+  message(paste("dropping table",fileNames))
+  for(i in seq_len(length(fileNames))){
+    sqliteQuickSQL(con, paste("DROP TABLE IF EXISTS",fileNames[i],sep=""))
+  }
+}
+
+
+
 
 #########################################################################
 ## Generate the database using the helper functions:
 #########################################################################
 
 generateOrgDbFromNCBI <- function(tax_id){
-  ## TODO: "smallTEST.sqlite" is obviously not ok.
-  con <- dbConnect(SQLite(), "smallTEST.sqlite")
+  require(RSQLite)
+  require(GO.db)
+  ## TODO: "9606_TEST.sqlite" is obviously not ok.
+  con <- dbConnect(SQLite(), paste(tax_id,"_TEST.sqlite",sep=""))
   
   ## I need a list of files, along with their column names 
   ## (needed for schema definitions later)
   ## IF ANY OF THESE gets moved in the source files
   ## then things will be in the wrong place!
-  files = list("gene2go.gz" = c("tax_id","gene_id","go_id","evidence",
-               "go_qualifier", "go_description","pubmed_id","category"),
-            "gene2pubmed.gz" = c("tax_id","gene_id", "pubmed_id"),
-    ## "gene2accession.gz" = c("tax_id","gene_id","status","rna_accession",
-    ##     "rna_gi","protein_accession","protein_gi","genomic_dna_accession",
-    ##     "genomic_dna_gi","genomic_start","genomic_end","orientation",
-    ##     "assembly"),
+  files = list(
+    "gene2pubmed.gz" = c("tax_id","gene_id", "pubmed_id"),
+    "gene2accession.gz" = c("tax_id","gene_id","status","rna_accession",
+        "rna_gi","protein_accession","protein_gi","genomic_dna_accession",
+        "genomic_dna_gi","genomic_start","genomic_end","orientation",
+        "assembly"),
     ## This one might be needed later
-            "gene2refseq.gz" = c("tax_id","gene_id","status","rna_accession",
-              "rna_gi","protein_accession","protein_gi","genomic_dna_accession",
-              "genomic_dna_gi","genomic_start","genomic_end","orientation",
-              "assembly"),
-            "gene2unigene.gz" = c("gene_id","unigene_id"),
-            "gene_info.gz" = c("tax_id","gene_id","symbol","locus_tag",
-              "synonyms","dbXrefs","chromosome","map_location","description",
-              "gene_type","nomenclature_symbol","nomenclature_name",
-              "nomenclature_status","other_designations"),
+    "gene2refseq.gz" = c("tax_id","gene_id","status","rna_accession",
+        "rna_gi","protein_accession","protein_gi","genomic_dna_accession",
+        "genomic_dna_gi","genomic_start","genomic_end","orientation",
+        "assembly"),
+    "gene2unigene.gz" = c("gene_id","unigene_id"),
+    "gene_info.gz" = c("tax_id","gene_id","symbol","locus_tag",
+        "synonyms","dbXrefs","chromosome","map_location","description",
+        "gene_type","nomenclature_symbol","nomenclature_name",
+        "nomenclature_status","other_designations"),
     ##        "mim2gene.gz" = c("mim_id","gene_id","relation_type"),
-    ## only useful for human (already covered)
-            "gene_refseq_uniprotkb_collab.gz" = c("refseq_id","uniprot_id")
+    ##        "gene_refseq_uniprotkb_collab.gz" = c("refseq_id","uniprot_id"),
+    "gene2go.gz" = c("tax_id","gene_id","go_id","evidence",
+        "go_qualifier", "go_description","pubmed_id","category")
   )
 
   
   ## TEMP commented for speedup
-  ##.makeBaseDBFromDLs(files, tax_id, con)
+  .makeBaseDBFromDLs(files, tax_id, con)
   
-  ## TEMP For testing only:
-  tax_id = "9606"  
+  ## TEMP For post DB testing only:
+  ##tax_id = "9606"  
 
   
   ## Make the central table
@@ -362,7 +466,7 @@ generateOrgDbFromNCBI <- function(tax_id){
   rsp <- rs[rs[,3]!="-",c(1,3)]
   colnames(rsp)[2] <- "accession"
   rs <- rbind(rsr,rsp)
-  rs[,2] = sub("\\.\\d+$","",rs[,2])
+  rs[,2] <- sub("\\.\\d+$","",rs[,2])
   .makeSimpleTable(rs, table="refseq", con)
   
   ## unigene 
@@ -388,3 +492,30 @@ generateOrgDbFromNCBI <- function(tax_id){
  ## 3) make all necessary glue/mods here to enable their usage
  ## 4) keep things generic so that I can use them again for biomaRt stuff
 ## 5) Add code to "name" the database correctly. (get rid of "TEST.sqlite")
+
+## 6) Test the whole thing in one run.
+
+## 7) Add Metadata to the DB. (including MAPCOUNTS where relevant)
+
+## 8) Make it so that I can automatically make the package around the DB.
+## 9) Don't forget to redirect the getData functs to point to NCBI
+## 10) Right now there is a problem when there is nothing in a table (like no GO terms) - done?
+
+
+## generateOrgDbFromNCBI("9606")
+## generateOrgDbFromNCBI("1428") ## Bacillus thuringiensis?
+## Still don't kwow which kind ... Bacillus thuringiensis?
+
+## try zebrafinch?
+
+## generateOrgDbFromNCBI("59729")
+
+## 11) Some organisms don't have GO terms.  Enable getting these from blast2GO
+## when they are MIA. - done?
+## To do it, all I need to do is take advantage of the fact that blast2GO
+## follows this convention for their files:
+
+## http://bioinfo.cipf.es/b2gfar/_media/species:data:9913.annot.zip?id=species%3A9913&cache=cache
+## Therefore:
+## url = paste("http://bioinfo.cipf.es/b2gfar/_media/species:data:",
+##      tax_id,".annot.zip?id=species%3A",tax_id,"&cache=cache",sep="")
