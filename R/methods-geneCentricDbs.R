fe2f045e715bec79b6b78bdfd2291bf80df45806
Marc Carlson <mcarlson@fhcrc.org>
2011-09-28 23:02:35 +0000

    minor update as I begin to add methods for new objects.
    OrgDb, ChipDb, GODb and InparanoidDb all need methods defined.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@58545 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-geneCentricDbs.R
+++ R/methods-geneCentricDbs.R
@@ -1,24 +1,115 @@
-## Select Methods
+## Select Methods return the results from looking up things (cols) that match
+## the keys provided.  cols is a character vector to specify columns the user
+## wants back and keys are the keys to look up.
 
-.select <- function(con, keys, cols){
-  dbGetQuery(con, "SELECT * FROM genes")
+.select <- function(con, keys, cols, idType){  
+  sql <- paste("SELECT * FROM ",
+               paste(cols, collapse=","),
+               " WHERE ", idType ," IN ('",
+               paste(keys, collapse="','"), "') ",
+               sep="")
+  dbGetQuery(con, sql)
 }
 
+## ## lets just have it merge() bimaps
+## .select <- function(con, keys, cols, idType){  
+
+## }
+
+
+
 setMethod("select", "OrgDb",
-    function(db, keys, cols) .select(dbConn(db), keys, cols)
+    function(db, keys, cols) .select(dbConn(db), keys, cols, "gene_id")
 )
 
 
 setMethod("select", "ChipDb",
-    function(db, keys, cols) .select(dbConn(db), keys, cols)
+    function(db, keys, cols) .select(dbConn(db), keys, cols, "probe_id")
 )
 
 
-setMethod("select", "InparanoidDb",
-    function(db, keys, cols) .select(dbConn(db), keys, cols)
+## setMethod("select", "InparanoidDb",
+##     function(db, keys, cols) .select(dbConn(db), keys, cols, "gene_id")
+## )
+
+
+setMethod("select", "GODb",
+    function(db, keys, cols) .select(dbConn(db), keys, cols, "go_id")
 )
 
 
 ## library(org.Tguttata.eg.db)
 ## ls(2)
 ## select(Tguttata_eg_OrgDb)
+
+
+
+## Cols methods return the list of things that users can ask for.  This can be
+## just the table names, or it might be a list of mappings
+
+.cols <- function(con){
+  dbListTables(con)## TEMP: Not what we actually want
+}
+
+
+setMethod("cols", "OrgDb",
+    function(x) .cols(dbConn(x))
+)
+
+
+setMethod("cols", "ChipDb",
+    function(x) .cols(dbConn(x))
+)
+
+
+## setMethod("cols", "InparanoidDb",
+##     function(x) .cols(dbConn(x))
+## )
+
+
+setMethod("cols", "GODb",
+    function(x) .cols(dbConn(x))
+)
+
+
+
+## Keys methods return the possible primary keys.  So for EG based packages,
+## this will be the viable entrez gene IDs.
+
+## .keys <- function(con){
+##   as.character(t(dbGetQuery(con, "SELECT gene_id FROM genes")))
+## }
+
+setMethod("keys", "OrgDb",
+    function(x) as.character(t(dbGetQuery(dbConn(x),
+                                           "SELECT gene_id FROM genes"))) 
+)
+
+
+setMethod("keys", "ChipDb",
+    function(x) as.character(t(dbGetQuery(dbConn(x),
+                                           "SELECT probe_id FROM probes")))
+)
+
+
+## setMethod("keys", "InparanoidDb",
+##     function(x) as.character(t(dbGetQuery(dbConn(x),
+##                                            "SELECT gene_id FROM genes")))
+## )
+
+
+setMethod("keys", "GODb",
+    function(x) as.character(t(dbGetQuery(dbConn(x),
+                                           "SELECT go_id FROM go_term")))
+)
+
+
+
+
+## library(org.Hs.eg.db)
+## con = AnnotationDbi:::dbConn(org.Hs.eg.db)
+## keys = keys(org.Hs.eg.db)[1:5]
+## cols= "genes"
+## select(org.Hs.eg.db, keys, cols)
+
+## idType = "gene_id"
