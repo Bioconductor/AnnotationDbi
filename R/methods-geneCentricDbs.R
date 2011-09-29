f10089f253ae6a31b03098e725b7912fa45e7af5
Marc Carlson <mcarlson@fhcrc.org>
2011-09-29 21:12:28 +0000

    Updates the cols() for GODb, ChipDb, OrgDb
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@58563 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-geneCentricDbs.R
+++ R/methods-geneCentricDbs.R
@@ -2,7 +2,8 @@
 ## the keys provided.  cols is a character vector to specify columns the user
 ## wants back and keys are the keys to look up.
 
-.select <- function(con, keys, cols, idType){  
+.select <- function(db, keys, cols, idType){
+  con <- dbConn(db)
   sql <- paste("SELECT * FROM ",
                paste(cols, collapse=","),
                " WHERE ", idType ," IN ('",
@@ -19,93 +20,110 @@
 
 
 setMethod("select", "OrgDb",
-    function(db, keys, cols) .select(dbConn(db), keys, cols, "gene_id")
+    function(db, keys, cols) .select(db, keys, cols, "gene_id")
 )
-
 
 setMethod("select", "ChipDb",
-    function(db, keys, cols) .select(dbConn(db), keys, cols, "probe_id")
+    function(db, keys, cols) .select(db, keys, cols, "probe_id")
 )
 
+setMethod("select", "GODb",
+    function(db, keys, cols) .select(db, keys, cols, "go_id")
+)
 
 ## setMethod("select", "InparanoidDb",
-##     function(db, keys, cols) .select(dbConn(db), keys, cols, "gene_id")
+##     function(db, keys, cols) .select(db, keys, cols, "gene_id")
 ## )
 
 
-setMethod("select", "GODb",
-    function(db, keys, cols) .select(dbConn(db), keys, cols, "go_id")
-)
 
-
+## TEST CODE:
 ## library(org.Tguttata.eg.db)
 ## ls(2)
 ## select(Tguttata_eg_OrgDb)
 
 
 
+
+
+
+
+
+
+
+
+##############################################################################
 ## Cols methods return the list of things that users can ask for.  This can be
 ## just the table names, or it might be a list of mappings
 
-.cols <- function(con){
-  dbListTables(con)## TEMP: Not what we actually want
+
+.cols <- function(db){
+  meta <- metadata(db) ##bug I need to get7 the package name!
+  schema <- meta[meta["name"] == "DBSCHEMA","value"]
+  objList <- eval(parse(text=paste("AnnotationDbi:::",schema,
+                  "_AnnDbBimap_seeds",sep="")))  
+  unlist(lapply(objList, function(x){x$objName}))
 }
 
 
 setMethod("cols", "OrgDb",
-    function(x) .cols(dbConn(x))
+    function(x) .cols(x)
 )
-
 
 setMethod("cols", "ChipDb",
-    function(x) .cols(dbConn(x))
+    function(x) .cols(x)
 )
 
+setMethod("cols", "GODb",
+    function(x) .cols(x)
+)
 
+## something more tricky required for Inparanoid since a single template
+## exists for all (basically I need to do.call() a specific function
 ## setMethod("cols", "InparanoidDb",
-##     function(x) .cols(dbConn(x))
+##     function(x) .cols(x)
 ## )
 
 
-setMethod("cols", "GODb",
-    function(x) .cols(dbConn(x))
-)
+
+
+
+
+
+
+
+
 
 
 
 ## Keys methods return the possible primary keys.  So for EG based packages,
 ## this will be the viable entrez gene IDs.
 
-## .keys <- function(con){
-##   as.character(t(dbGetQuery(con, "SELECT gene_id FROM genes")))
-## }
-
 setMethod("keys", "OrgDb",
     function(x) as.character(t(dbGetQuery(dbConn(x),
                                            "SELECT gene_id FROM genes"))) 
 )
-
 
 setMethod("keys", "ChipDb",
     function(x) as.character(t(dbGetQuery(dbConn(x),
                                            "SELECT probe_id FROM probes")))
 )
 
+setMethod("keys", "GODb",
+    function(x) as.character(t(dbGetQuery(dbConn(x),
+                                           "SELECT go_id FROM go_term")))
+)
 
+## for Inparanoid, we want to select keys carefully (depeninding on the
+## organism - which is in the metadata)
 ## setMethod("keys", "InparanoidDb",
 ##     function(x) as.character(t(dbGetQuery(dbConn(x),
 ##                                            "SELECT gene_id FROM genes")))
 ## )
 
 
-setMethod("keys", "GODb",
-    function(x) as.character(t(dbGetQuery(dbConn(x),
-                                           "SELECT go_id FROM go_term")))
-)
 
-
-
-
+## TEST CODE:
 ## library(org.Hs.eg.db)
 ## con = AnnotationDbi:::dbConn(org.Hs.eg.db)
 ## keys = keys(org.Hs.eg.db)[1:5]
