45c9cbfc594251a088461e7fc66f838f7e81eaba
Marc Carlson <mcarlson@fhcrc.org>
2012-04-18 22:46:44 +0000

    Adds helper functions to help with the seqnames.db package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@65259 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-seqnames.R
+++ R/methods-seqnames.R
@@ -1,12 +1,19 @@
 ###############################################################################
 ## Code to access the special chromosome name conventions DB.
 
-supportedSeqnameStyles <- function(){
+## setup for accessing the seqnames DB
+.seqnamesSetup <- function(){
   require("RSQLite")
   require("seqnames.db")
   ## get a connection to this
   db <- system.file("extdata","seqnames.sqlite",package="seqnames.db")
   con <- dbConnect("SQLite", dbname=db)
+  con
+}
+
+## A discovery method for users to learn the supported seqname styles
+supportedSeqnameStyles <- function(){
+  con <- .seqnamesSetup()
   tables <- dbListTables(con)
   getFields <- function(table, con){
     fields <- dbListFields(con, table)
@@ -15,3 +22,32 @@ supportedSeqnameStyles <- function(){
   names(fields) <- tables
   fields
 }
+
+
+## A function for retrieving one type of data
+## usage: extractSeqnameSet("NCBI", "Rattus norvegicus")
+extractSeqnameSet <- function(style="UCSC", species="Homo sapiens"){
+  oriSpecies <- species
+  species <- sub(" ", "_", species)
+  con <- .seqnamesSetup()
+  ## quick test:
+  supNames <- supportedSeqnameStyles()
+  if(any(style %in% supNames[[species]])){
+    sql <- paste("SELECT",style,"FROM", species)
+    result <- as.character(t(dbGetQuery(con,sql)))
+  }else{stop("The style specified by '",style,
+             "' does not have a compatible entry for the species ",oriSpecies)}
+  result
+}
+
+
+## A function for testing if a vector of data is the correct thing
+## "TRUE" Usage example
+## testSeqnames(seqnames=c(1:20,"X","MT"),"NCBI","Rattus norvegicus")
+## "FALSE" Usage example
+## testSeqnames(seqnames=c(1:22,"X","Y","MT"),"NCBI","Rattus norvegicus")
+testSeqnames <- function(seqnames, style="ensembl", species="Homo sapiens"){
+  trueSeq <- extractSeqnameSet(style=style,species=species)
+  identical(seqnames, trueSeq)
+}
+
