0a37483a7bca662eb1b784df98a453880e3f2e49
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-09-25 17:59:11 +0000

    Argument of 'synchronous' option to RSQLite::dbConnect() changed to 'off'. This is compatible with current RSQLite but also forward-compatible with RSQLite 1.0.0. All annotation packages must be rebuilt with this version of AnnotationDbi!
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@94536 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AnnDbPkg-templates-common.R
+++ R/AnnDbPkg-templates-common.R
@@ -12,7 +12,7 @@ dbFileConnect <- function(dbfile)
     ## We should not need to explicitly library(RSQLite) because it's in
     ## Depends and Imports but this seems to make 'R CMD check hgu95av2.db' happier.
     library(RSQLite)
-    dbConnect(SQLite(), dbname=dbfile, cache_size=64000, synchronous=0,
+    dbConnect(SQLite(), dbname=dbfile, cache_size=64000, synchronous="off",
               flags=SQLITE_RO)
 }
 
