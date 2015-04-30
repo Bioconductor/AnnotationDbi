4b6834e09a3996505544f8c94a7da1081b788fcc
Marc Carlson <mcarlson@fhcrc.org>
2015-04-30 17:57:57 +0000

    Adds alternate (and shorter) testing package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@103320 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/test_AnnotationDbi_package.R
+++ R/test_AnnotationDbi_package.R
@@ -1,1 +1,5 @@
+## if(!suppressWarnings(library('org.testing.db',logical.return=TRUE))){
+##     install.packages(system.file('extdata','org.testing.db', package='AnnotationDbi'), repos=NULL)
+## }
+
 .test <- function() BiocGenerics:::testPackage("AnnotationDbi")
