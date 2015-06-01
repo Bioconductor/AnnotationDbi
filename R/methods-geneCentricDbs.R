1e7024306e6ff4ace1f0e36bbb349076db718b6f
Marc Carlson <mcarlson@fhcrc.org>
2015-06-01 22:43:00 +0000

    A more complete solution for my recurring problem with results that come back as factors from mapIds.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@104447 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-geneCentricDbs.R
+++ R/methods-geneCentricDbs.R
@@ -1709,10 +1709,7 @@ setMethod("keytypes", "GODb",
     ## TODO: remove the suppressWarnings() call once you get rid of that warning
     res <- select(x, keys=keys, columns=column, keytype=keytype) 
     ## then split accordingly (and return sorted by initial keys)
-    res <- split(res[[column]], f=res[[keytype]])[keys]
-    if(keytype==column){
-        res <- lapply(res, as.character)
-    }
+    res <- as.character(split(res[[column]], f=res[[keytype]])[keys])
     ## internal helper to toss out multiply matching things
     .filtMults <- function(data){
         idx <- sapply(data, FUN=function(x){
