6b8e8b94cd8b905c5bf2158d1672d66e18f3f832
Herve Pages <hpages@fhcrc.org>
2017-01-23 07:16:08 +0000

    don't use 'vsf="unix-none"' on Windows
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationDbi@126087 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -11,19 +11,25 @@ dbFileConnect <- function(dbfile)
     if (!file.exists(dbfile))
         stop("DB file '", dbfile, "' not found")
 
-    ## 'vsf="unix-none"' added on 01/11/2017 to avoid the infamous "database
-    ## is locked" error that some users have reported on a few occasions in
-    ## the past 10 years (and that we were never able to reproduce). Last time
-    ## it was reported was on 01/11/2017 by Rob Bradley at Fred Hutch. Rob was
-    ## trying to access TxDb.Hsapiens.UCSC.hg19.knownGene from multiple nodes
-    ## on the Hutch cluster where TxDb.Hsapiens.UCSC.hg19.knownGene is
+    ## Use of 'vsf="unix-none"' added on 01/11/2017 to avoid the infamous "da-
+    ## tabase is locked" error that some users have reported on a few occasions
+    ## in the past 10 years (and that we were never able to reproduce). Last
+    ## time it was reported was on 01/11/2017 by Rob Bradley at Fred Hutch.
+    ## Rob was trying to access TxDb.Hsapiens.UCSC.hg19.knownGene from multiple
+    ## nodes on the Hutch cluster where TxDb.Hsapiens.UCSC.hg19.knownGene is
     ## installed on an NFS file system. Many thanks to Ben McGough, System
     ## Administrator at the Hutch, for investigating this and suggesting the
     ## 'vsf="unix-none"' solution.
     ## See ?`dbConnect,SQLiteDriver-method` and http://www.sqlite.org/vfs.html
     ## for more information about the the SQLite OS Interface or "VFS".
-    dbConnect(SQLite(), dbname=dbfile, cache_size=64000L, 
-              synchronous="off", flags=SQLITE_RO, vfs="unix-none")
+    if (.Platform$OS.type == "unix") {
+        dbConnect(SQLite(), dbname=dbfile, cache_size=64000L, 
+                  synchronous="off", flags=SQLITE_RO, vfs="unix-none")
+    } else {
+        ## Use default 'vfs' on Windows.
+        dbConnect(SQLite(), dbname=dbfile, cache_size=64000L, 
+                  synchronous="off", flags=SQLITE_RO)
+    }
 }
 
 ### Used at unload time (in .onUnload) by all SQLite-based ann data packages.
